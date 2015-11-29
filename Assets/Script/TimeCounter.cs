b8f5e78e6d42d4a5b9fdcd6e0571e225494705cc
krok1029 <krok1029@gmail.com>
2015-11-30 02:36:28 +0800

    initial

---
+++ Assets/Script/TimeCounter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimeCounter : MonoBehaviour {
+    public static int time;
+
+    Text text;
+
+	void Awake ()
+    {
+        text = GetComponent<Text> ();
+        time = 0;
+    }
+    		
+	
+	void Update () {
+        text.text = "Time: " + time;
+	}
+}
