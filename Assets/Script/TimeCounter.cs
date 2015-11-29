947637b172f7516a8058dd2d98070cb76bebbbe9
krok1029 <krok1029@gmail.com>
2015-11-30 03:08:53 +0800

    TIMER DONE

--- Assets/Script/TimeCounter.cs
+++ Assets/Script/TimeCounter.cs
@@ -4,17 +4,22 @@ using UnityEngine.UI;
 
 public class TimeCounter : MonoBehaviour {
     public static int time;
-
+    int timer_i = 0;
     Text text;
 
-	void Awake ()
+	void Start()
     {
+        InvokeRepeating("timer", 1f, 1f);
         text = GetComponent<Text> ();
         time = 0;
     }
-    		
+    void timer()
+    {
+        timer_i += 1;
+        Debug.Log(timer_i);
+    }
 	
 	void Update () {
-        text.text = "Time: " + time;
-	}
+        text.text = "TIME: "+ timer_i;
+    }
 }
